27ffef7fd59e70e638747df21141090d09b8b793
michael3674 <168384616+michael3674@users.noreply.github.com>
2025-01-04 01:54:40 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250104/나눗셈의 나머지/remainder-of-division.cpp
+++ 250104/나눗셈의 나머지/remainder-of-division.cpp
@@ -7,8 +7,8 @@ int main() {
     cin >> n1 >> n2;
     int arr[10] = {};
     while (n1 > 1){
-        n1 = n1 / n2;
         arr[n1 % n2]++;
+        n1 = n1 / n2;
     }
     int total = 0;
     for (int i = 0; i < n2; i++){
